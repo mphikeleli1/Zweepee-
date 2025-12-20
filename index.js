833283b638669684ab6e4a50093dba5173f387e0
mphikeleli1 <busanigama@gmail.com>
2025-12-20 13:40:01 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ app.get('/webhook', (req, res) => {
     const token = req.query['hub.verify_token'];
     const challenge = req.query['hub.challenge'];
 
-    // Debug lines
     console.log('Token from URL:', token);
     console.log('Token from ENV:', process.env.WA_VERIFY_TOKEN);
 
     if (token === process.env.WA_VERIFY_TOKEN) {
+        res.setHeader("Content-Type", "text/plain");
         res.status(200).send(challenge);
     } else {
         res.status(403).send('Invalid token');
