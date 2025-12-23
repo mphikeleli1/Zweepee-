c3dae7cce2bba752913adc49cd37f4dca3f9e1b8
mphikeleli1 <busanigama@gmail.com>
2025-12-23 23:31:49 +0200

    Update webhook.js

--- webhook.js
+++ webhook.js
@@ -1,1 +1,15 @@
+export default function handler(req, res) {
+  const token = req.query['hub.verify_token'];
+  const challenge = req.query['hub.challenge'];
 
+  if (req.method === 'GET' && token === 'zweepee2025') {
+    return res.status(200).send(challenge);
+  }
+
+  if (req.method === 'POST') {
+    console.log('Incoming webhook body:', req.body);
+    return res.status(200).send('OK');
+  }
+
+  res.status(404).send('Not found');
+}
