b5b6bb9532e5dfedfed59ab219fc95cea7654e5c
mphikeleli1 <busanigama@gmail.com>
2025-12-23 22:11:58 +0200

    Update webhook.js

--- webhook.js
+++ webhook.js
@@ -1,4 +1,1 @@
-export default function handler(req, res) {
-  if (req.method === 'GET') return res.send('Verification failed');
-  if (req.method === 'POST') return res.status(200).send('OK');
-}
+
